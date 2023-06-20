20b06ad3f78a7edff50cf202007d41086005f359
Augusto Cuccione <cuccioneaugusto@gmail.com>
2023-06-20 18:19:18 -0300

    agregados productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Header from "./components/header/header";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div>
       <Header />
-      <Button variant="danger">click me</Button>
+      <ItemListContainer />
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,82 @@
 import { compile } from "sass";
 import "./ItemListContainer.css";
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import BaseComponent from "bootstrap/js/dist/base-component";
 
-const ItemListContainer = ({ mensaje }) => {
+const MOCK_DATA = [
+  {
+    id: 1,
+    nombre: "producto 1",
+    descripcion: "fsdamflkasndisdojfioas",
+    precio: 1500,
+    img: "https://dummyimage.com/200x200/000/fff",
+  },
+  {
+    id: 2,
+    nombre: "producto 2",
+    descripcion: "fsdamflkasndisdojfioas",
+    precio: 1500,
+    img: "https://dummyimage.com/200x200/000/fff",
+  },
+  {
+    id: 3,
+    nombre: "producto 3",
+    descripcion: "fsdamflkasndisdojfioas",
+    precio: 1500,
+    img: "https://dummyimage.com/200x200/000/fff",
+  },
+  {
+    id: 4,
+    nombre: "producto 4",
+    descripcion: "fsdamflkasndisdojfioas",
+    precio: 1500,
+    img: "https://dummyimage.com/200x200/000/fff",
+  },
+  {
+    id: 5,
+    nombre: "producto 5",
+    descripcion: "fsdamflkasndisdojfioas",
+    precio: 1500,
+    img: "https://dummyimage.com/200x200/000/fff",
+  },
+  {
+    id: 6,
+    nombre: "producto 6",
+    descripcion: "fsdamflkasndisdojfioas",
+    precio: 1500,
+    img: "https://dummyimage.com/200x200/000/fff",
+  },
+];
+
+const PedirDatos = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(MOCK_DATA);
+    }, 2000);
+  });
+};
+
+const ItemListContainer = () => {
+  const [Productos, setProductos] = useState([]);
+  console.log(Productos);
+
+  useEffect(() => {
+    PedirDatos()
+      .then((res) => {
+        setProductos(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="listcontainer">
       <h2>Item List Container</h2>
       <hr />
-      <p>{mensaje}</p>
+      {Productos.length === 0
+        ? "no hay productos todavia"
+        : "se cargaron los productos"}
     </div>
   );
 };
